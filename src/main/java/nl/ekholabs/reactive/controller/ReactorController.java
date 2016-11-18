8173616b78437c199a22d3b7e0f3896aa015dbfe
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2016-11-19 00:09:35 +0100

    Add WebClient in order to achieve real non-blocking communication.

--- src/main/java/nl/ekholabs/reactive/controller/ReactorController.java
+++ src/main/java/nl/ekholabs/reactive/controller/ReactorController.java
@@ -3,10 +3,13 @@ package nl.ekholabs.reactive.controller;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.reactive.ClientRequest;
+import org.springframework.web.client.reactive.WebClient;
 import reactor.core.publisher.Mono;
 
 import static java.util.logging.Logger.getLogger;
@@ -23,7 +26,8 @@ public class ReactorController {
   @Value("${ping.url:http://example.com}")
   private String url;
 
-  private RestTemplate restTemplate = new RestTemplate();
+  private final RestTemplate restTemplate = new RestTemplate();
+  private final WebClient client = WebClient.create(new ReactorClientHttpConnector());
 
   @RequestMapping("/parallelism/{max}")
   public Mono<Result> parallelism(@PathVariable final int max) {
@@ -36,9 +40,28 @@ public class ReactorController {
     return range(1, max) // <1>
       .log() //
       .flatMap( // <2>
-        value -> fromCallable(() -> status(value)) // <3>
+        value -> fromCallable(() -> blockingStatus(value)) // <3>
           .subscribeOn(elastic()), // <4>
         concurrency) // <5>
+      .collect(() -> new Result(), (result, status) -> result.add(status)) // <6>
+      .doOnSuccess((newResult) -> newResult.stop()); // <7>
+
+    // <1> make 10 calls
+    // <2> drop down to a new publisher to process in parallel
+    // <3> blocking code here inside a Callable to defer execution
+    // <4> subscribe to the slow publisher on a background thread
+    // <5> concurrency hint in flatMap
+    // <6> collect results and aggregate into a single object
+    // <7> at the end stop the clock
+  }
+
+  @RequestMapping("/nonblocking/{max}")
+  public Mono<Result> nonblocking(@PathVariable final int max) {
+    log.info("Handling /nonblocking");
+
+    return range(1, max) // <1>
+      .log() //
+      .flatMap((value) -> nonblockingStatus()) // <5>
       .collect(() -> new Result(), (result, status) -> result.add(status)) // <6>
       .doOnSuccess((newResult) -> newResult.stop()); // <7>
 
@@ -57,7 +80,7 @@ public class ReactorController {
     return range(1, max) // <1>
       .log() //
       .map( // <2>
-        (value) -> status(value)) // <3>
+        (value) -> blockingStatus(value)) // <3>
       .collect(() -> new Result(), (result, status) -> result.add(status)) // <4>
       .doOnSuccess((newResult) -> newResult.stop()) // <5>
       .subscribeOn(parallel()); // <6>
@@ -70,7 +93,13 @@ public class ReactorController {
     // <6> subscribe on a background thread
   }
 
-  private HttpStatus status(int value) {
+  private HttpStatus blockingStatus(final int value) {
     return this.restTemplate.getForEntity(url, String.class, value).getStatusCode();
+  }
+
+  private Mono<HttpStatus> nonblockingStatus() {
+    final ClientRequest<Void> request = ClientRequest.GET(url).build();
+
+    return client.exchange(request).then(clientResponse -> Mono.fromCallable(() -> clientResponse.statusCode()));
   }
 }
